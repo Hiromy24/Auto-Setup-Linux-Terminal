e6479dd40f09469711d3b5ad0caf973767656735
Hiromy24 <henryillescas24@gmail.com>
2025-10-14 18:32:29 +0200

    minor change

--- arch.sh
+++ arch.sh
@@ -28,6 +28,7 @@ sudo pacman -S --noconfirm "${pacman_packages[@]}"
 yay -S --noconfirm "${aur_packages[@]}"
 
 git clone --depth 1 https://github.com/Hiromy24/Dotfiles ~/dotfiles
+rm -rf .config/hypr
 cd ~/dotfiles
 stow cava 
 stow hypr 
