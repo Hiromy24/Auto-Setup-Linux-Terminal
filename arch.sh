b811130e325dbb604ae0e81637260d1ef72678ef
Hiromy24 <henryillescas24@gmail.com>
2025-10-14 16:56:22 +0200

    init commit

---
+++ arch.sh
@@ -0,0 +1,43 @@
+set -euo pipefail
+
+sudo pacman -Syu --noconfirm
+
+sudo sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+sudo locale-gen
+sudo localectl set-locale LANG=en_US.UTF-8
+
+sudo pacman -S --noconfirm --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay makepkg -si --noconfirm
+cd ~
+rm -rf ~/yay
+
+pacman_packages= (
+	ghostty lf neovim rofi stow
+
+	firefox dolphin
+
+	zsh
+)
+aur_packages= (
+	wl-kbptr
+
+	spotify
+
+	hyprshade hyprpaper 
+
+	pokemon-colorscripts-git
+)
+
+sudo pacman -S --noconfirm "${pacman_packages[@]}"
+yay -S --noconfirm "${aur_packages[@]}"
+
+git clone --depth 1 https://github.com/Hiromy24/Dotfiles ~/dotfiles
+cd ~/dotfiles
+stow -t ~ .
+cd ~
+
+echo "shell"
+ZSH_PATH="$(which zsh)"
+grep -qxF "$ZSH_PATH" /etc/shells || echo "$ZSH_PATH" | sudo tee -a /etc/shells
+chsh -s "$ZSH_PATH"
